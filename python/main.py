b49aeb59a26ae0a54534c827f3d12a34509ffad2
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-09-28 16:37:18 +0900

    Make user inputs interactive

--- python/main.py
+++ python/main.py
@@ -2,10 +2,8 @@ import sys
 sys.path.append('./src')
 from application import Application
 
-try:
-    _, dirname, filename, mode, *_ = sys.argv
-except ValueError:
-    _, dirname, filename, *_ = sys.argv
-    mode                     = '-d'
+dirname = input('Provide the directory which contains files you would like to delete: ')
+pattern = input('Provide the dirname or filename pattern you would like to delete: ')
+mode    = input('Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e(dry_run) once is strongly recommended: ')
 
-Application(dirname = dirname, filename = filename, mode = mode).run()
+Application(dirname = dirname, pattern = pattern, mode = mode).run()
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,4 +1,13 @@
 require_relative './src/application'
 
 dirname, filename, mode, *_ = ARGV
-Application.run(dirname:, filename:, mode:)
+puts 'Provide the directory which contains files you would like to delete'
+dirname = gets.chomp
+
+puts 'Provide the dirname or filename pattern you would like to delete'
+pattern = gets.chomp
+
+puts 'Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e(dry_run) once is strongly recommended'
+mode = gets.chomp
+
+Application.run(dirname:, pattern:, mode:)
