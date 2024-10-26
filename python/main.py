daa8dd99b282a848398faec91d109cdf8c2f1a2a
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-26 12:43:39 +0900

    Update interactive message

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ from application import Application
 
 dirname = input('Provide the directory which contains files you would like to delete: ')
 pattern = input('Provide the dirname or filename pattern you would like to delete: ')
-mode    = input('Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e once is strongly recommended: ')
+mode    = input('Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!: ')
 
 Application(dirname = dirname, pattern = pattern, mode = mode).run()
 
--- ruby/main.rb
+++ ruby/main.rb
@@ -7,7 +7,7 @@ dirname = gets.chomp
 puts 'Provide the dirname or filename pattern you would like to delete'
 pattern = gets.chomp
 
-puts 'Provide -e(execution) if you would truly like to delete the files. This operation is cannot be undone, so trying to run without -e once is strongly recommended'
+puts 'Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!'
 mode = gets.chomp
 
 Application.run(dirname:, pattern:, mode:)
