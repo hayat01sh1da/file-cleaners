e97f1aa08908253005e548566e7bdd32d2bc9cd5
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-12-31 03:41:34 +0900

    Remove whitespace from command-line user interactive inputs

--- python/main.py
+++ python/main.py
@@ -5,9 +5,9 @@ import glob
 sys.path.append('./src')
 from application import Application
 
-dirname = input('Provide the directory which contains files you would like to delete: ')
-pattern = input('Provide the dirname or filename pattern you would like to delete: ')
-mode    = input('Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!: ')
+dirname = input('Provide the directory which contains files you would like to delete: ').strip()
+pattern = input('Provide the dirname or filename pattern you would like to delete: ').strip()
+mode    = input('Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!: ').strip()
 
 params = dict()
 for key, value in { 'dirname': dirname, 'pattern': pattern, 'mode': mode }.items():
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,13 +1,13 @@
 require_relative './src/application'
 
 puts 'Provide the directory which contains files you would like to delete'
-dirname = gets.chomp
+dirname = gets.chomp.strip
 
 puts 'Provide the dirname or filename pattern you would like to delete'
-pattern = gets.chomp
+pattern = gets.chomp.strip
 
 puts 'Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!'
-mode = gets.chomp
+mode = gets.chomp.strip
 
 params = { dirname:, pattern:, mode: }.reject { |_, value| value.empty? }
 
