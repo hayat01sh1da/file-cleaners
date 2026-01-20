2f27c76999091b678d011353f17dd20a4598a316
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-01-20 16:45:25 +0900

    Cache exec_mode

--- python/src/application.py
+++ python/src/application.py
@@ -11,6 +11,7 @@ class Application:
         self.pattern = pattern
         self.mode    = mode
         self.files   = glob.glob(os.path.join(dirname, '**', pattern), recursive = True)
+        self.exec_mode = self.__exec_mode__()
         self.env     = inspect.stack()[1].filename.split('/')[-2]
 
     def run(self):
