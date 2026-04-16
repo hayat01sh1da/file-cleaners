0a2dbf9b218c0678ce37994d0d7bb7681915f129
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-17 02:08:50 +0900

    Fix: [error] Type `(::String | nil)` does not have method `empty?`

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,14 +3,14 @@
 require_relative './src/application'
 
 puts 'Provide the directory which contains files you would like to delete'
-dirname = gets.chomp.strip
+dirname = gets&.chomp&.strip
 
 puts 'Provide the dirname or filename pattern you would like to delete'
-pattern = gets.chomp.strip
+pattern = gets&.chomp&.strip
 
 puts 'Provide d(dry_run: default) to make sure what directories and files are to be delete first. Then, provide e(execution) if you would truly like to delete the files. This operation is cannot be undone, so be alert to your operation!'
-mode = gets.chomp.strip
+mode = gets&.chomp&.strip
 
-params = { dirname:, pattern:, mode: }.reject { |_, value| value.empty? }
+params = { dirname:, pattern:, mode: }.reject { |_, value| value&.empty? }
 
 Application.run(**params)
