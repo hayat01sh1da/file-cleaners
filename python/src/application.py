e4049cb8aa9af21c5304c94f42f000fb53cf097f
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-05-08 06:04:03 +0900

    Execute lint fixed with autopep8

--- python/src/application.py
+++ python/src/application.py
@@ -2,32 +2,57 @@ import os
 import glob
 import inspect
 
+
 class InvalidModeError(Exception):
     pass
 
+
 class Application:
-    def __init__(self, dirname: str = '.', pattern: str = '*', mode: str = 'd') -> None:
-        self.dirname: str   = dirname
-        self.pattern: str   = pattern
-        self.mode: str      = mode
-        self.files: list[str]     = glob.glob(os.path.join(dirname, '**', pattern), recursive = True)
+    def __init__(
+            self,
+            dirname: str = '.',
+            pattern: str = '*',
+            mode: str = 'd') -> None:
+        self.dirname: str = dirname
+        self.pattern: str = pattern
+        self.mode: str = mode
+        self.files: list[str] = glob.glob(
+            os.path.join(dirname, '**', pattern), recursive=True)
         self.exec_mode: str = self.__exec_mode__()
-        self.env: str       = inspect.stack()[1].filename.split('/')[-2]
+        self.env: str = inspect.stack()[1].filename.split('/')[-2]
 
     def run(self) -> None:
         self.__validate__()
         self.__output__(f'Target dirname is {os.path.abspath(self.dirname)}')
         if len(self.files) > 1:
-            self.__output__(f'========== [{self.__exec_mode__()}] Total File Count to Clean: {len(self.files)} ==========')
-            self.__output__(f'========== [{self.__exec_mode__()}] Start Cleaning {self.pattern} ==========')
+            self.__output__(
+                f'========== [{
+                    self.__exec_mode__()}] Total File Count to Clean: {
+                    len(
+                        self.files)} ==========')
+            self.__output__(
+                f'========== [{
+                    self.__exec_mode__()}] Start Cleaning {
+                    self.pattern} ==========')
             for file in self.files:
-                self.__output__(f'========== [{self.__exec_mode__()}] Cleaning {file} ==========')
+                self.__output__(
+                    f'========== [{self.__exec_mode__()}] Cleaning {file} ==========')
                 if self.mode == 'e':
                     os.remove(file)
-            self.__output__(f'========== [{self.__exec_mode__()}] Cleaned {self.pattern} ==========')
-            self.__output__(f'========== [{self.__exec_mode__()}] Total Cleaned File Count: {len(self.files)} ==========')
+            self.__output__(
+                f'========== [{
+                    self.__exec_mode__()}] Cleaned {
+                    self.pattern} ==========')
+            self.__output__(
+                f'========== [{
+                    self.__exec_mode__()}] Total Cleaned File Count: {
+                    len(
+                        self.files)} ==========')
         else:
-            self.__output__(f'========== [{self.__exec_mode__()}] No {self.pattern} Remains ==========')
+            self.__output__(
+                f'========== [{
+                    self.__exec_mode__()}] No {
+                    self.pattern} Remains ==========')
 
     # private
 
@@ -36,7 +61,8 @@ class Application:
             case 'd' | 'e':
                 return
             case _:
-                raise InvalidModeError(f'{self.mode} is invalid mode. Provide either `d`(default) or `e`.')
+                raise InvalidModeError(
+                    f'{self.mode} is invalid mode. Provide either `d`(default) or `e`.')
 
     def __exec_mode__(self) -> str:
         if self.mode == 'e':
