05b97f7317544f292d0a546bf6012366bf5e4280
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-05-15 06:10:31 +0900

    Add type annotations to satisfy mypy strict mode

--- python/test/test_application.py
+++ python/test/test_application.py
@@ -9,14 +9,14 @@ from application import Application, InvalidModeError
 PATTERN = '*.txt'
 
 
-def test_invalid_mode(tmp_dir):
+def test_invalid_mode(tmp_dir: str) -> None:
     with pytest.raises(InvalidModeError) as excinfo:
         Application(dirname=tmp_dir, pattern=PATTERN, mode='a').run()
     assert str(excinfo.value) == 'a is invalid mode. Provide either `d`(default) or `e`.'
 
 
 @pytest.mark.parametrize('mode', [None, 'd'])
-def test_run_in_dry_run_mode(tmp_dir, mode):
+def test_run_in_dry_run_mode(tmp_dir: str, mode: str | None) -> None:
     kwargs = {'dirname': tmp_dir, 'pattern': PATTERN}
     if mode is not None:
         kwargs['mode'] = mode
@@ -24,6 +24,6 @@ def test_run_in_dry_run_mode(tmp_dir, mode):
     assert len(glob.glob(os.path.join(tmp_dir, '**', PATTERN), recursive=True)) == 100
 
 
-def test_run_in_exec_mode(tmp_dir):
+def test_run_in_exec_mode(tmp_dir: str) -> None:
     Application(dirname=tmp_dir, pattern=PATTERN, mode='e').run()
     assert len(glob.glob(os.path.join(tmp_dir, '**', PATTERN), recursive=True)) == 0
